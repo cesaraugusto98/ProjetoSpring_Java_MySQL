3c673287072c8a522e257d580950d6773cb40158
cesaraugusto98 <cesaraugusto.santos@outlook.com>
2018-09-21 20:27:21 -0300

    Criacao do projeto

---
+++ DemoIntroducaoJpa/src/main/java/edu/fatec/sjc/DemoIntroducaoJpa/DemoIntroducaoJpaApplication.java
@@ -0,0 +1,12 @@
+package edu.fatec.sjc.DemoIntroducaoJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoIntroducaoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoIntroducaoJpaApplication.class, args);
+	}
+}
